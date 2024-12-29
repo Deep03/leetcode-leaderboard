ede5c0d0c43063b8670dfb9a71fe13c02fe42058
Deep Lekhak <59480224+Deep03@users.noreply.github.com>
2024-12-28 20:09:01 -0500

    wrote get_user_score()

--- backend/script.js
+++ backend/script.js
@@ -1,6 +1,7 @@
 const leetcode_url = "https://leetcode.com/graphql";
+const { error } = require("console");
 const fs = require("fs");
-const parsed_json = JSON.parse(fs.readFileSync("user_query/aarjeet1j-query.json"));
+const parsed_json = JSON.parse(fs.readFileSync("user_query/arjeetjoshi175_query.json"));
 
 async function fetch_data() {
     try {
@@ -15,7 +16,7 @@ async function fetch_data() {
         if (!response.ok) throw new Error('HTTP error! Status: ' + response.status);
 
         const data = await response.json();
-        if (data.data) fs.writeFile('user_data/aarjeet1j-data.json', JSON.stringify(data.data, null, 2), (err) => {
+        if (data.data) fs.writeFile('user_data/arjeetjoshi175_data.json', JSON.stringify(data.data, null, 2), (err) => {
             if (err) console.log("Error writing to file:", err);
             else console.log("File has been written.");
         });
@@ -25,9 +26,25 @@ async function fetch_data() {
     }
 }
 
-function rank_data() {
-    
+export function user_score(user_name) {
+    let file_name  = 'user_data/'+ user_name + '_data.json'
+    const data = JSON.parse(fs.readFileSync(file_name))
+    try {
+        const problems_solved_user = data.userProfileUserQuestionProgressV2.numAcceptedQuestions
+        const easy_solved = problems_solved_user[0].count
+        const medium_solved = problems_solved_user[1].count
+        const hard_solved = problems_solved_user[2].count
+        const rank = easy_solved * 1 + medium_solved * 2 + hard_solved * 2
+        return rank
+    } catch (error) {
+        console.log('Error: ', error)
+        return -1
+    }
+
+
+
 }
 
-fetch_data();
-rank_data();
+// fetch_data();
+let name = 'panditapollo'
+user_score(name);
